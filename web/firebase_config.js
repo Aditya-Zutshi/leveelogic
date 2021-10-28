7e8f5510f33bfa78a097216856d8c3e88bf8dbab
Rob van Putten <breinbaasnl@gmail.com>
2021-10-28 17:57:57 +0200

    merged 2 files to 1

--- web/firebase_config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB5dJXKq33YcJsmh7LAcBpFPgr7eSDtVvs",
-  authDomain: "leveelogic-1ce15.firebaseapp.com",
-  projectId: "leveelogic-1ce15",
-  storageBucket: "leveelogic-1ce15.appspot.com",
-  messagingSenderId: "399942395743",
-  appId: "1:399942395743:web:3f6c1531ab6b4a196bf8ea"
-};
